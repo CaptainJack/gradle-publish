6c9f8c81c6b42c7fa6db7749f52eefb58120d0f4
Alexey Chuprin <a.chuprin@capjack.ru>
2021-05-11 18:01:13 +0300

    Develop

--- src/main/kotlin/ru/capjack/gradle/publisher/PublisherPlugin.kt
+++ src/main/kotlin/ru/capjack/gradle/publisher/PublisherPlugin.kt
@@ -3,9 +3,13 @@ package ru.capjack.gradle.publisher
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.getByName
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.mavenCapjackPrivate
 import org.gradle.kotlin.dsl.mavenCapjackPublic
@@ -22,11 +26,47 @@ class PublisherPlugin : Plugin<Project> {
 		
 		project.afterEvaluate {
 			configure<PublishingExtension> {
-				val reposit = rootProject.extensions.getByType<RepositExtension>()
-				val publish = extensions.getByType<PublisherExtension>()
-				when (publish.scope.get()) {
-					PublisherExtension.Scope.PUBLIC  -> repositories.mavenCapjackPublic(reposit)
-					PublisherExtension.Scope.PRIVATE -> repositories.mavenCapjackPrivate(reposit)
+				configurePublishingRepositories(this@afterEvaluate, this)
+				configurePublishingPublications(this@afterEvaluate, this)
+			}
+		}
+	}
+	
+	private fun configurePublishingRepositories(project: Project, publishing: PublishingExtension) {
+		if (publishing.repositories.isEmpty()) {
+			val reposit = project.rootProject.extensions.getByType<RepositExtension>()
+			val publish = project.extensions.getByType<PublisherExtension>()
+			when (publish.scope.get()) {
+				PublisherExtension.Scope.PUBLIC  -> publishing.repositories.mavenCapjackPublic(reposit)
+				PublisherExtension.Scope.PRIVATE -> publishing.repositories.mavenCapjackPrivate(reposit)
+			}
+		}
+	}
+	
+	private fun configurePublishingPublications(project: Project, publishing: PublishingExtension) {
+		if (publishing.publications.isEmpty()) {
+			val publicationName = project.name
+				.split('_', '-')
+				.joinToString("", transform = String::capitalize)
+				.decapitalize()
+			
+			if (project.pluginManager.hasPlugin("org.gradle.java")) {
+				var sourcesJar = project.tasks.findByName("sourcesJar")
+				
+				if (sourcesJar == null) {
+					sourcesJar = project.tasks.create<Jar>("sourcesJar") {
+						archiveClassifier.set("sources")
+						from(project.extensions.getByName<SourceSetContainer>("sourceSets")["main"].allSource)
+					}
+				}
+				
+				publishing.publications.create<MavenPublication>(publicationName) {
+					artifactId = project.name
+					groupId = project.group.toString()
+					version = project.version.toString()
+					
+					from(project.components["java"])
+					artifact(sourcesJar)
 				}
 			}
 		}
