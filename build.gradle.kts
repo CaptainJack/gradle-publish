f22292f0fcc97b6809e35729e72dec712a19182f
Alexey Chuprin <a.chuprin@capjack.ru>
2023-05-17 19:07:45 +0300

    Gradle 8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
 	`kotlin-dsl`
-	`java-gradle-plugin`
-	`maven-publish`
-	id("com.gradle.plugin-publish") version "0.15.0"
+	id("com.gradle.plugin-publish") version "1.2.0"
 	id("nebula.release") version "15.3.1"
 }
 
@@ -15,23 +13,20 @@ repositories {
 
 dependencies {
 	implementation(kotlin("gradle-plugin"))
-	implementation("ru.capjack.gradle:gradle-reposit:1.0.0")
+	implementation("ru.capjack.gradle:gradle-reposit:1.1.0")
 	implementation("com.netflix.nebula:nebula-release-plugin:15.3.1")
 }
 
 gradlePlugin {
+	vcsUrl.set("https://github.com/CaptainJack/gradle-publish")
+	website.set(vcsUrl.get())
 	plugins.create("CapjackPublisher") {
 		id = "ru.capjack.publisher"
 		implementationClass = "ru.capjack.gradle.publisher.PublisherPlugin"
 		displayName = "CapjackPublisher"
+		description = "Provides publishing to CaptainJack repositories"
+		tags.set(listOf("capjack"))
 	}
-}
-
-pluginBundle {
-	vcsUrl = "https://github.com/CaptainJack/gradle-publish"
-	website = vcsUrl
-	description = "Provides publishing to CaptainJack repositories"
-	tags = listOf("capjack")
 }
 
 tasks["postRelease"].dependsOn("publishPlugins")
